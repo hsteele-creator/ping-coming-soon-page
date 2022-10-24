32a6d2175919fa9c20a5ccb1dd3ecc91dda17d23
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-23 20:46:55 -0400

    finished the project

--- script.js
+++
@@ -1,9 +0,0 @@
-const formInput = document.querySelector("#subscribe")
-const emailInput = document.querySelector(".email")
-const provideEmailMsg = document.querySelector("#provide-email")
-
-formInput.addEventListener("submit", function(e) {
-    e.preventDefault()
-    provideEmailMsg.classList.toggle("hidden")
-
-})
