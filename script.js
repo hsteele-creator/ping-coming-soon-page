b414d7d85f67edeb1bea296273857f3e2f4fdfd3
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-27 15:09:45 -0400

    finished project

---
+++ script.js
@@ -0,0 +1,25 @@
+const emailInput = document.querySelector("#email")
+const emailWarning = document.querySelector("#provide-email")
+const notifyButton = document.querySelector("#notify")
+
+const valid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+
+
+notifyButton.addEventListener("click", function(e) {
+
+    e.preventDefault()
+
+    console.log("hello")
+    if(!emailInput.value.match(valid)) {
+        emailWarning.classList.remove("hidden")
+    } else {
+        emailWarning.classList.add("hidden")
+    }
+
+})
+
+
+// notifyButton.addEventListener("submit", function() {
+//     emailWarning.classList.toggle("hidden")
+// })
+
